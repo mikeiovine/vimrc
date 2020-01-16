4b7cb07e2e38778b518d49e64f8e29bac897c333
Michael Iovine <mike.iovine7@gmail.com>
2020-01-15 21:53:18 -0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-cp .vimrc ~/.vimrc
-cp -aT .vim ~/.vim
-echo "Installed to ~/.vimrc and ~/.vim/"
+#!/bin/sh
+INSTALL_DIR=/home/$USER/Documents
+
+cd "$INSTALL_DIR"
+git clone https://github.com/mikeiovine/vimrc.git
+cd vimrc
+
+ln -s "$INSTALL_DIR/vimrc/.vim" /home/$USER/.vim
+ln -s "$INSTALL_DIR/vimrc/.vimrc" /home/$USER/.vimrc
+echo "Installed to $INSTALL_DIR"
+
